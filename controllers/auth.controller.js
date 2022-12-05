8e85382c20dbc276d429d09679d7e7a06885b845
vizmarvizcaino <gatome2000@hotmail.com>
2022-12-05 15:23:33 -0500

    first commit backend1

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.js";
+import { User } from "../models/User.js";
 import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
